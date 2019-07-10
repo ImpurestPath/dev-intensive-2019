7b4f0aff7b7f93aa21e510d4b79feca0211f0aff
Vladimir Romaniuk <romvr99@yandex.ru>
2019-07-10 20:40:46 +0300

    fix parse and other

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -14,45 +14,48 @@ enum class TimeUnits {
     HOUR,
     DAY;
 
-    fun plural(number: Int): String {
-        val name: Array<String> = when (this) {
-            SECOND -> arrayOf("секунду", "секунды", "секунд")
-            MINUTE -> arrayOf("минуту", "минуты", "минут")
-            HOUR -> arrayOf("час", "часа", "часов")
-            DAY -> arrayOf("день", "дня", "дней")
-        }
-        val nameFin: String
-
-        if (number in 0..20) {
-            nameFin = when (number) {
-                1 -> name[0]
-                in 2..4 -> name[1]
-                else -> name[2]
+    fun plural(value: Int): String {
+        when (this){
+            SECOND -> if (value % 100 in 11..19)
+                return "$value секунд"
+            else {
+                when(value % 10) {
+                    1 -> return "$value секунду"
+                    in 2..4 -> return "$value секунды"
+                    in 5..9, 0 -> return "$value секунд"
+                }
             }
-            return "$number $nameFin"
-        }
-
-        val numberString: String = number.toString()
-        val last2digits = numberString.drop(numberString.length - 2)
-
-        if (last2digits.toInt() in 0..20) {
-            nameFin = when (last2digits.toInt()) {
-                1 -> name[0]
-                in 2..4 -> name[1]
-                else -> name[2]
+            MINUTE -> if (value % 100 in 11..19)
+                return "$value минут"
+            else {
+                when (value % 10) {
+                    1 -> return "$value минуту"
+                    in 2..4 -> return "$value минуты"
+                    in 5..9, 0 -> return "$value минут"
+                }
             }
-            return "$number $nameFin"
+            HOUR -> if (value % 100 in 11..19)
+                return "$value часов"
+            else {
+                when (value % 10) {
+                    1 -> return "$value час"
+                    in 2..4 -> return "$value часа"
+                    in 5..9, 0 -> return "$value часов"
+                }
+            }
+            DAY -> if (value % 100 in 11..19)
+                return "$value дней"
+            else {
+                when (value % 10) {
+                    1 -> return "$value день"
+                    in 2..4 -> return "$value дня"
+                    in 5..9, 0 -> return "$value дней"
+                }
+            }
         }
-        val last1Digit = last2digits.drop(1).toInt()
-
-        nameFin = when (last1Digit) {
-            1 -> name[0]
-            in 2..4 -> name[1]
-            else -> name[2]
-        }
-
-        return "$number $nameFin"
+        return "";
     }
+
 }
 
 fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String {
@@ -74,184 +77,27 @@ fun Date.add(value: Int, units: TimeUnits): Date {
 }
 
 fun Date.humanizeDiff(date: Date = Date()): String {
-    val diff = (this.time - date.time)
-    var timeString = " "
-
-    if (diff in -75000..75000) {
-        when (diff / 1000) {
-            in -1..1 -> return "только что"
-            in 2..45 -> return "через несколько секунд"
-            in -45..-2 -> return "несколько секунд назад"
-            in -75..-46 -> return "минуту назад"
-            in 46..75 -> return "через минуту"
-        }
+    val diff = (date.time - this.time)
+    val seconds = diff / SECOND
+    var output = ""
+    output += when (diff / SECOND) {
+        in -1L..1L -> "только что"
+        in 1L..45L -> "несколько секунд назад"
+        in -45L..-1L -> "через несколько секунд"
+        in 45L..75L -> "минуту назад"
+        in -75L..-45L -> "через минуту"
+        in 75L..2700L -> "${diff / MINUTE} ${if (seconds < 120) "минуту" else if (seconds < 300) "минуты" else "минут"} назад"
+        in -2700L..-75L -> "через ${-diff / MINUTE} ${if (seconds > -120) "минуту" else if (seconds > -300) "минуты" else "минут"}"
+        in 2700L..4500L -> "час назад"
+        in -4500L..-2700L -> "через час"
+        in 4500L..79200L -> "${diff / HOUR} ${if (seconds < 7200) "час" else if (seconds < 18000) "часа" else "часов"} назад"
+        in -79200L..-4500L -> "через ${-diff / HOUR} ${if (seconds > -7200) "час" else if (seconds > -18000) "часа" else "часов"}"
+        in 79200L..93600L -> "день назад"
+        in -93600L..-79200L -> "через день"
+        in 93600L..31104000L -> "${diff / DAY} ${if (seconds < 172800) "день" else if (seconds < 432000) "дня" else "дней"} назад"
+        in -31104000L..-93600L -> "через ${-diff / DAY} ${if (seconds > -172800) "день" else if (seconds > -432000) "дня" else "дней"}"
+        in 31104000L..Long.MAX_VALUE -> "более года назад"
+        else -> "более чем через год"
     }
-
-    if (diff in -2759000..2759000) {
-        val intDiffMin: Int = (diff.toInt()) / 1000 / 60
-
-        if (intDiffMin >= -20 || intDiffMin <= 20) {
-            when (intDiffMin) {
-                in -20..-5 -> timeString = "минут"
-                in -4..-2 -> timeString = "минуты"
-                in -1..1 -> timeString = "минуту"
-                in 2..4 -> timeString = "минуты"
-                in 5..20 -> timeString = "минут"
-            }
-        }
-
-        if (intDiffMin in -45..-21 || intDiffMin in 21..45) {
-            val toMinutes = intDiffMin / 1000 / 60
-            val lastCharMinutes: Char = toMinutes.toString().last()
-            timeString = when ((lastCharMinutes.toString().toInt())) {
-                1 -> "минуту"
-                in 2..4 -> "минуты"
-                else -> "минут"
-            }
-        }
-
-        return if (intDiffMin > 0) {
-            "через $intDiffMin $timeString"
-        } else {
-            ("${intDiffMin.toString().drop(1)} $timeString назад")
-        }
-    }
-
-    if (diff in -4559000..4559000) {
-        return if (diff > 0) {
-            "через час"
-        } else {
-            "час назад"
-        }
-    }
-
-    if (diff in -96199999..-4560000 || diff in 4560000..96199999) {
-        val toHours: Int = ((diff / 3600).toInt()) / 1000
-        when (toHours) {
-            in -26..-23 -> return "день назад"
-            -22 -> timeString = "часа"
-            -21 -> timeString = "час"
-            in -20..-5 -> timeString = "часов"
-            in -4..-2 -> timeString = "часа"
-            in -1..1 -> timeString = "час"
-            in 2..4 -> timeString = "часа"
-            in 5..20 -> timeString = "часов"
-            21 -> timeString = "час"
-            22 -> timeString = "часа"
-            in 23..26 -> return "через день"
-        }
-        return if (toHours > 0) {
-            "через $toHours $timeString"
-        } else {
-            "${toHours.toString().drop(1)} $timeString назад"
-        }
-    }
-
-    val numLen: Int
-    var toDays = (((diff / 3600) / 1000) / 24).toInt()
-    val lastTwoOfThree: Int
-    val lastofTwo: Int
-    var moreThenZero = true
-
-    if (diff <= -97200000 || diff >= 97200000) {
-
-        if (toDays < 0) {
-            numLen = toDays.toString().drop(1).length
-            toDays = toDays.toString().drop(1).toInt()
-            moreThenZero = false
-        } else {
-            numLen = toDays.toString().length
-        }
-        if (toDays > 360) {
-            return if (moreThenZero) {
-                "более чем через год"
-            } else {
-                "более года назад"
-            }
-        }
-
-        if (toDays.toString().drop(1) == "00") {
-            timeString = "дней"
-            return if (moreThenZero) {
-                "через $toDays $timeString"
-            } else {
-                "$toDays $timeString назад"
-            }
-        }
-
-        if (numLen == 1) {
-            timeString = when (toDays) {
-                1 -> "день"
-                in 2..4 -> "дня"
-                else -> "дней"
-            }
-            return if (moreThenZero) {
-                "через $toDays $timeString"
-            } else {
-                "$toDays $timeString назад"
-            }
-        }
-        if (numLen == 2) {
-            lastofTwo = toDays.toString().drop(1).toInt()
-            lastTwoOfThree = toDays
-
-            if (lastTwoOfThree in 1..20) {
-                when (lastTwoOfThree) {
-                    1 -> timeString = "день"
-                    in 2..4 -> timeString = "дня"
-                    in 5..20 -> timeString = "дней"
-                }
-
-                return if (moreThenZero) {
-                    "через $toDays $timeString"
-                } else {
-                    "$toDays $timeString назад"
-                }
-            }
-            timeString = when (lastofTwo) {
-                1 -> "день"
-                in 2..4 -> "дня"
-                else -> "дней"
-            }
-            return if (moreThenZero) {
-                "через $toDays $timeString"
-            } else {
-                "$toDays $timeString назад"
-            }
-        }
-
-        if (numLen == 3) {
-            lastTwoOfThree = toDays.toString().drop(1).toInt()
-            if (lastTwoOfThree in 0..20) {
-                when (lastTwoOfThree) {
-                    0 -> timeString = "дней"
-                    1 -> timeString = "день"
-                    in 2..4 -> timeString = "дня"
-                    in 5..20 -> timeString = "дней"
-                }
-                return if (moreThenZero) {
-                    "через $toDays $timeString"
-                } else {
-                    "$toDays $timeString назад"
-                }
-            }
-            lastofTwo = lastTwoOfThree.toString().drop(1).toInt()
-
-            timeString = when (lastofTwo) {
-                1 -> "день"
-                in 2..4 -> "дня"
-                else -> "дней"
-            }
-            return if (moreThenZero) {
-                "через $toDays $timeString"
-            } else {
-                "$toDays $timeString назад"
-            }
-
-        }
-
-        return "$toDays $timeString"
-    } else {
-        return (diff / 3600).toString()
-    }
+    return output
 }
--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,41 +1,18 @@
 package ru.skillbranch.devintensive.extensions
 
-fun String.truncate(lenght: Int = 16): String {
-    var trimString = this.trim()
-
-    if (lenght == trimString.length) {
-        return trimString
-    }
-
-    if (lenght >= trimString.length) {
-        return trimString
-    }
-
-    trimString = trimString.substring(0, lenght)
-
-    if ((trimString.last()).isWhitespace()) {
-        trimString = trimString.dropLast(1)
-        trimString = "$trimString..."
-        return trimString
-    }
-
-    return "$trimString..."
+fun String.stripHtml(): String {
+    val escape = "&[^;]+;".toRegex()
+    val space = " {2,}".toRegex()
+    val tag = "<[^>]+>".toRegex()
+    return this.replace(escape, "").replace(space, " ").replace(tag, "")
 }
 
-fun String.stripHtml(): String {
-    var inputHtml: String = Regex("""<.*?>""").replace(this, "")
-
-    inputHtml = inputHtml.replace("""&amp;""", "")
-    inputHtml = inputHtml.replace("""&gt;""", "")
-    inputHtml = inputHtml.replace("""&lt;""", "")
-    inputHtml = inputHtml.replace("""&#39;""", "")
-    inputHtml = inputHtml.replace("""&quot;""", "")
-
-    if (inputHtml.contains("\n")) {
-        inputHtml = Regex("""\\s+""").replace(inputHtml, " ")
-        inputHtml = Regex(" {2}").replace(inputHtml, " ")
-    } else {
-        inputHtml = Regex("\\s+").replace(inputHtml, " ")
+fun String.truncate(num: Int = 16): String {
+    val outString = this.trim()
+    if (num > outString.length - 1)
+        return outString
+    return when (outString[num - 1]) {
+        ' ' -> "${outString.substring(0 until num - 1)}..."
+        else -> "${outString.substring(0 until num)}..."
     }
-    return inputHtml
 }
